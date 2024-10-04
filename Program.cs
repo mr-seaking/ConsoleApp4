ea9e72548adfc82536336cdd624ed85ee2bcce12
mr-seaking <pavelshuvalov@live.com>
2024-10-04 12:25:34 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+//using System.ComponentModel.Design;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int[] myArray = new int[01];
+        
+        Num_enter(ref myArray, myArray.Length);
+        
+    }
+
+    static void Resize(ref int[] array, int newSize)
+    {
+        int[] newArray = new int[newSize];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            newArray[i] = array[i];     
+        }
+        //array = newArray;
+        //Console.WriteLine(newArray.Length);
+        Num_enter(ref newArray, array.Length);
+    }
+    static void Num_enter(ref int[] myArray, int i)
+    {
+        
+        Console.WriteLine("установлен размер массива: " + myArray.Length);
+
+        for (int j = 0+i; j < myArray.Length; j++)
+        {
+            
+            
+            Console.WriteLine("Введите число:");
+            string? input = Console.ReadLine();
+            if (int.TryParse(input, out int num))
+            {
+                myArray[j] = num;
+            }
+
+
+        }
+        Resize(ref myArray, (myArray.Length + 1)*2);
+    }
+}
